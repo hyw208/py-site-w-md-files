fde40844c134e0bc676923dd0d095e99a1e9d249
hyw208 <hyw208@gmail.com>
2025-01-03 00:41:59 -0500

    add comment

--- routers/browse.py
+++ routers/browse.py
@@ -4,6 +4,7 @@ from utils.files import get_html_content
 
 router = APIRouter()
 
+# this has to be the last router to include
 @router.get("/{file_path:path}", response_class=HTMLResponse)
 def get_html(file_path: str):
     return get_html_content(file_path)
